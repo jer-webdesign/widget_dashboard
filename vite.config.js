f347f2e118aa18109e60e039555d4433d5a2bfa1
Jerry <jeremy.g.olanda@gmail.com>
2025-08-27 19:56:34 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/widget_dashboard/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/widget_dashboard/' : '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
